5e601d30c260aaf0c830971fc7ced52bddee7e33
truong.ngoc <truong.ngoc>
2022-11-14 11:08:50 +0700

    update gitignore

--- node_modules/bitcoinjs-lib/src/templates/witnesscommitment/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.output = void 0;
-const output = require('./output');
-exports.output = output;
--- node_modules/bitcoinjs-lib/src/templates/witnesspubkeyhash/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.output = exports.input = void 0;
-const input = require('./input');
-exports.input = input;
-const output = require('./output');
-exports.output = output;
